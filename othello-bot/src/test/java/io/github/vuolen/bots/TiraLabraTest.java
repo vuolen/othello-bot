9070b4f5e70f80bc7713478f442d7788fcdf52f8
Lennu Vuolanne <vuolanne.lennu@gmail.com>
2020-09-10 21:16:19 +0300

    Edited template pom.xml, added bot skeleton and test suite skeleton

---
+++ othello-bot/src/test/java/io/github/vuolen/bots/TiraLabraTest.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package io.github.vuolen.bots;
+
+/**
+ *
+ * @author Lennu Vuolanne <vuolanne.lennu@gmail.com>
+ */
+public class TiraLabraTest {
+    
+}
